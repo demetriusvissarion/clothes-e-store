c9a50472d3cfe4f9427f0c933a1fb0e9698d33f9
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-01-19 13:11:18 +0000

    rendered inventory

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./app/App";
 import store from "./app/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App state={store.getState()} dispatch={store.dispatch} />
-  </React.StrictMode>
-);
+const render = () => {
+  ReactDOM.render(
+    <App state={store.getState()} dispatch={store.dispatch} />,
+    document.getElementById("root")
+  );
+};
+render();
 
+// Subscribe render to the store.
+store.subscribe(render);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
